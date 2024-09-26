6b6607481acb3d1213b1bcbe941c156199f256cc
oit-rajat-silwal <154954589+oit-rajat-silwal@users.noreply.github.com>
2024-09-26 11:08:46 +0530

    added suspense to zohocallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Suspense } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,9 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
       >
-        <Suspense>
-          {children}
-        </Suspense>s
+
+        {children}
+
       </body>
     </html>
   );
